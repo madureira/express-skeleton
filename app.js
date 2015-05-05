447b0f8f25ef924b6c9b343d70df06ec02d633ab
Rafael Madureira dos Santos <rafael.santos2@walmart.com>
2015-05-05 18:28:02 -0300

    Restructuring the architecture

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express'),
 
 
 app.use(express.static(__dirname + '/public'));
-app.use(require('./src/controllers'));
+app.use(require('./src/main/controllers'));
 
 app.listen(3000, function() {
     console.log('Listening on port 3000...');
